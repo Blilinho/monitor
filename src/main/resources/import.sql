64a90c41ee9dd61aa4f2401debeaafb98e880c57
Blinho <vasmichels@gmail.com>
2026-09-23 09:42:00 -0300

    import update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,13 +5,13 @@
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
 
-insert into Monitor (name, brand) values('UltraSharp U2720Q', 'Dell');
-insert into Monitor (name, brand) values('UltraGear 27GL850', 'LG');
-insert into Monitor (name, brand) values('Odyssey G7', 'Samsung');
-insert into Monitor (name, brand) values('ROG Swift PG259QN', 'ASUS');
-insert into Monitor (name, brand) values('Predator X34', 'Acer');
-insert into Monitor (name, brand) values('AORUS FI27Q', 'Gigabyte');
-insert into Monitor (name, brand) values('ProArt Display PA278QV', 'ASUS');
-insert into Monitor (name, brand) values('Mobiuz EX2510', 'BenQ');
-insert into Monitor (name, brand) values('ThinkVision P27h-20', 'Lenovo');
-insert into Monitor (name, brand) values('Alienware AW2521H', 'Dell');
+insert into Monitor (name, brand, panelType) values('UltraSharp U2720Q', 'Dell', 'LED');
+insert into Monitor (name, brand, panelType) values('UltraGear 27GL850', 'LG', 'OLED');
+insert into Monitor (name, brand, panelType) values('Odyssey G7', 'Samsung', 'QLED');
+insert into Monitor (name, brand, panelType) values('ROG Swift PG259QN', 'ASUS', 'LED');
+insert into Monitor (name, brand, panelType) values('Predator X34', 'Acer', 'IPS');
+insert into Monitor (name, brand, panelType) values('AORUS FI27Q', 'Gigabyte', 'TN');
+insert into Monitor (name, brand, panelType) values('ProArt Display PA278QV', 'ASUS', 'LED');
+insert into Monitor (name, brand, panelType) values('Mobiuz EX2510', 'BenQ', 'IPS');
+insert into Monitor (name, brand, panelType) values('ThinkVision P27h-20', 'Lenovo', 'TN');
+insert into Monitor (name, brand, panelType) values('Alienware AW2521H', 'Dell', 'OLED');
